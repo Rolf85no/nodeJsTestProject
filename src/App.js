0eca7a210cf083747cdf1a28bd11ce7d482b1496
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-09-16 14:07:27 +0200

    Find and findall working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+  const [backEndData, setBackEndData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [users, setUsers] = React.useState(null);
+  const [choosenUser, setChoosenUser] = React.useState("");
+  const url = "http://10.2.0.228:3000/api/v1/posts"
+
+  React.useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setBackEndData(apiData.posts);
+        const uniqueSet = new Set(apiData.posts.map(item => item.username))
+        const uniqueUsers = [...uniqueSet]
+        setUsers(uniqueUsers);
+      }
+      catch (err) {
+        console.log(err);
+        setBackEndData(null)
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+
+    getUsers();
+  }, [])
+
+  const getUserPost = async (event) => {
+    try {
+      setLoading(true)
+      setChoosenUser(event.target.value);
+      const res = await fetch(`${url}/${event.target.value}`)
+      const apiData = await res.json();
+      setBackEndData(apiData.posts);
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+    finally {
+      setLoading(false);
+    }
+
+  }
+
+  const postsElements = !loading
+    ?
+    backEndData.map(item => {
+      return (
+        <div key={item._id}>
+          <h3>{item.username}</h3>
+          <p>{item.post}</p>
+
+        </div>
+
+      )
+    })
+
+    :
+
+    <div> loading </div>
+
+  const uniqueUsersEl = !loading ? users.map(user => <option key={user} value={user}> {user}</option>) : ''
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <h2>Posts:</h2>
+      <div>
+        <h3>Choose poster:</h3>
+        <select onChange={getUserPost} value={choosenUser}>
+          <option value={""}> All</option>
+          {uniqueUsersEl}
+        </select>
+      </div>
 
-export default App;
+      {postsElements}
+    </main>
+  )
+}
